6aa8080f18b948e9db7d9d8315a99aedf0f28115
Murylo Firmino <murylomf@users.noreply.github.com>
2020-05-15 19:13:29 -0300

    aula14

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -40,3 +40,64 @@ p {
 }
 
 `;
+
+export const IssueList = styled.ul`
+padding-top: 30px;
+margin-top: 30px;
+border-top: 1px solid #eee;
+list-style: none;
+
+li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+    margin-top: 10px;
+}
+
+img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+}
+
+div{
+    flex: 1;
+    margin-left: 15px;
+}
+
+strong {
+    font-size: 14px;
+    a{
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+            color: #7159c1;
+        }
+    }
+
+    span{
+        background: #95c8;
+        color: black;
+        border-radius: 9px;
+        font-size: 12px;
+        font-weight: 600;
+        height: 20px;
+        line-height: 15px;
+        padding: 3px 4px;
+        margin-left: 10px;
+    }
+}
+p {
+        margin-top: 5px;
+        font-size: 13px;
+        color: #999;
+    }
+}
+
+
+`;
